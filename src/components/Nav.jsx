ce5044383cee25d0640d701c2fd1dcd67f9bb3ba
Pedro <pedrocastillo24.pc@gmail.com>
2023-03-26 19:35:16 -0300

    completed project

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="w-[100%] bg-dark h-[3rem] text-primary flex items-center justify-center gap-4 mb-[5rem] ">
-      <a href="/">ARTICLES</a>
-      <a href="https://portfolio-mocha-omega-74.vercel.app/" target="_blank">
+    <nav className="w-[100%] bg-dark h-[4rem] text-primary flex items-center justify-center gap-6 mb-[5rem] ">
+      <a href="/" className="hover:underline">
+        ARTICLES
+      </a>
+      <a
+        href="https://portfolio-mocha-omega-74.vercel.app/"
+        className="hover:underline"
+        target="_blank"
+      >
         PORTFOLIO
       </a>
-      <a href="https://github.com/Pedro29v" target="_blank">
+      <a
+        href="https://github.com/Pedro29v"
+        className="hover:underline"
+        target="_blank"
+      >
         GITHUB
       </a>
     </nav>
